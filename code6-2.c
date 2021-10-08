c39d7a16a9ac62f93d96a212149cef4c0edbfce5
SoNgSeUNgMiN96 <tmd9544@gmail.com>
2021-10-08 15:08:02 +0900

    environ 변수 활용하여 ,환경변수 출력해보기

---
+++ code6-2.c
@@ -0,0 +1,16 @@
+#include <stdlib.h> /* viewenv.c */
+#include <stdio.h>
+
+extern char **environ;	//환경변수 배열을 가져온다.
+
+int main()
+{
+	char **env = environ;		//문자배열에 포인터에 주소를 복사해온다..
+
+	while(*env){	//요소가있는동안.
+		printf("%s/n",*env);	//환경변수를 출력한다.
+		env++;
+	}
+	exit(0);
+}
+
