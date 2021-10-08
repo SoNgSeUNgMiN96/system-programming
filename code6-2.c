269d8654c6f7358f8c41f6cffe281917e3eb7749
SoNgSeUNgMiN96 <tmd9544@gmail.com>
2021-10-08 15:14:09 +0900

    /n으로 낸 오타를 \n으로 수정

--- code6-2.c
+++ code6-2.c
@@ -8,7 +8,7 @@ int main()
 	char **env = environ;		//문자배열에 포인터에 주소를 복사해온다..
 
 	while(*env){	//요소가있는동안.
-		printf("%s/n",*env);	//환경변수를 출력한다.
+		printf("%s\n",*env);	//환경변수를 출력한다.
 		env++;
 	}
 	exit(0);
